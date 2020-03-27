f4f31b1dc3f897b782bd5305dc95cb8cb05aae8b
N. Sanchirico <Lnk2past@gmail.com>
2020-03-27 16:47:25 -0400

    [DART-397] - add comments and documentation (#4)
    * comments and cleanup
    
    * formatting
    
    * update readme
    
    * update readme

--- pyconsumer/app.py
+++ pyconsumer/app.py
@@ -1,14 +1,21 @@
 import faust
 
 
+# create your application. here we give it a name and broker. the other two
+# arguments allow faust to find everything else neeeded
 app = faust.App('pyconsumer',
                 broker='kafka://kafka-broker-1:19092',
                 autodiscover=True,
                 origin='pyconsumer')
 
 
-import pyconsumer.streams.agents
+# this import seems weird, but we simply need to ensure that the agents (and
+# topics) are loaded explicitly after our app is created. faust's autodiscovery
+# takes care of the rest. in a more complex project this could be done more
+# cleanly
+import pyconsumer.streams.agents  # noqa: E402, F401
 
 
+# used for a main entrypoint
 def main() -> None:
     app.main()
--- pyconsumer/streams/agents.py
+++ pyconsumer/streams/agents.py
@@ -1,13 +1,16 @@
-from faust.streams import Stream
+from faust import StreamT
 from pyconsumer.app import app
 from pyconsumer.messages.stream_message import StreamMessage
 
 
+# create topics
 stream_out_topic = app.topic('stream.out', value_type=StreamMessage)
 
 
+# create an agent subscribed to the stream.out topic. this function receives
+# events, updates them, and then prints the event to stdout
 @app.agent(stream_out_topic)
-async def stream_out(stream : Stream):
+async def stream_out(stream: StreamT):
     """Print the events to stdout"""
     async for event in stream:
         event.breadcrumbs.append('python-kafka-consumer')
